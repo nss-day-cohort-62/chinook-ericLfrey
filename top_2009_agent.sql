30f7ea7fa3520a6813121468a5b0ae83e5c2fee9
Eric Frey <ericlfrey@gmail.com>
2023-05-09 18:27:29 -0500

    ex19. that was hard

---
+++ top_2009_agent.sql
@@ -0,0 +1,16 @@
+SELECT y.top_agent
+FROM(
+  SELECT MAX(x.total_sales), x.full_name AS top_agent
+    FROM (
+        SELECT
+            SUM(i.total) AS total_sales,
+            e.FirstName || ' ' || e.lastname AS full_name
+            FROM invoice AS i 
+            JOIN customer AS c 
+              ON c.customerid = i.customerid
+            JOIN employee AS e 
+              ON e.EmployeeId = c.SupportRepId
+            WHERE i.InvoiceDate LIKE '2009%'
+            GROUP BY e.EmployeeId
+        ) AS x
+    ) AS y
